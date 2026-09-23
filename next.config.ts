eed1ee8173bff2c100220f3418d899e544372979
Haeparic <v567v202@gmail.com>
2026-09-23 09:53:22 +0900

    [MODIFY] nextjs 정적 내보내기 관련 작업

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,12 @@
 import type { NextConfig } from "next";
 
+const isGitHubPagesBuild = process.env.GITHUB_ACTIONS === "true";
+
 const nextConfig: NextConfig = {
+  output: "export",
+  trailingSlash: true,
+  basePath: isGitHubPagesBuild ? "/intro" : "",
+  assetPrefix: isGitHubPagesBuild ? "/intro/" : "",
   reactCompiler: true,
   turbopack: {
     root: __dirname,
